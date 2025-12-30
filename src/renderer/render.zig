82d2d94795b4f68dc4244c2389881abbcbab0831
Hyyrokkin <moritz.witt@gmx.net>
2025-12-30 08:36:28 +0100

    no more drawing to a exure and the fps is now on screen and not in the tile

--- src/renderer/render.zig
+++ src/renderer/render.zig
@@ -7,42 +7,29 @@ const Rectangle = raylib.Rectangle;
 const screen_options = @import("settings").screen_options;
 
 pub fn render(updateFunction: fn () void) !void {
-    raylib.initWindow(screen_options.screen_width, screen_options.screen_height, "");
+    raylib.initWindow(screen_options.screen_width, screen_options.screen_height, screen_options.window_title);
     defer raylib.closeWindow();
 
     raylib.setTargetFPS(screen_options.target_fps);
 
-    const target: render_texture_2d = try raylib.loadRenderTexture(raylib.getScreenWidth(), raylib.getScreenHeight());
-    const rec: Rectangle = .{
-        .x = 0,
-        .y = 0,
-        .width = @floatFromInt(target.texture.width),
-        .height = @floatFromInt(-target.texture.height),
-    };
-
     while (!raylib.windowShouldClose()) {
-        raylib.beginTextureMode(target);
-        updateFunction();
-        raylib.endTextureMode();
-
         raylib.beginDrawing();
         defer raylib.endDrawing();
 
-        try redrawWindowTitle();
-
         raylib.clearBackground(raylib.Color.dark_gray);
 
-        raylib.drawTextureRec(target.texture, rec, raylib.Vector2.init(0, 0), raylib.Color.white);
+        updateFunction();
+        try drawFPS();
     }
 }
 
-fn redrawWindowTitle() !void {
+fn drawFPS() !void {
     const current_FPS: i32 = raylib.getFPS();
     const fps_string: []const u8 = try std.fmt.allocPrint(allocator, "{d}", .{current_FPS});
     defer allocator.free(fps_string);
 
-    const window_title: [:0]const u8 = try std.mem.concatWithSentinel(allocator, u8, &[_][]const u8{ screen_options.window_title_prefix, fps_string, screen_options.window_title_sufix }, 0);
-    defer allocator.free(window_title);
+    const fps_with: [:0]const u8 = try std.mem.concatWithSentinel(allocator, u8, &[_][]const u8{fps_string}, 0);
+    defer allocator.free(fps_with);
 
-    raylib.setWindowTitle(window_title);
+    raylib.drawText(fps_with, 10, 10, 10, raylib.Color.black);
 }
